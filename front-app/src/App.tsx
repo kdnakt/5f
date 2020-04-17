d8ad10ba9fd16c4c028b65743f2541c44d69e7d8
kdnakt <a.kid.1985@gmail.com>
2020-04-18 00:20:17 +0900

    call backend api

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App: React.FC = () => {
+  const [room, setRoom] = useState('');
+  useEffect(() => {
+    axios.get('/room').then(res => {
+      setRoom(res.data);
+    });
+  });
   return (
     <div className="App">
       <h3>5F app</h3>
+      <h5>{room}</h5>
     </div>
   );
 }
