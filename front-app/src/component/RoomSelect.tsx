ad3eb3698be0148f0c21537b2e63150e357f34fb
kdnakt <a.kid.1985@gmail.com>
2020-04-21 15:53:15 +0900

    show error for wrong input

--- front-app/src/component/RoomSelect.tsx
+++ front-app/src/component/RoomSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, FormEvent } from 'react';
 import {
   Button, FormGroup, FormControl, Form, FormLabel,
 } from 'react-bootstrap';
@@ -6,11 +6,21 @@ import axios from 'axios';
 
 const RoomSelect: React.FC = () => {
   const [selectedRoom, setSelectedRoom] = useState('');
+  const [roomIdInput, setRoomIdInput] = useState('');
+  const [roomNotExists, setRoomNotExists] = useState(false);
+
   const create = useCallback(() => {
     axios.get('/room/new').then(res => {
       setSelectedRoom(res.data);
     });
   }, []);
+  const enter = useCallback(() => {
+    axios.get(`/room?id=${roomIdInput}`).then(res => {
+      setSelectedRoom(res.data);
+    }).catch(_ => {
+      setRoomNotExists(true);
+    });
+  }, [roomIdInput]);
 
   if (selectedRoom) return <div>Room ID: {selectedRoom}</div>;
   return (
@@ -22,12 +32,28 @@ const RoomSelect: React.FC = () => {
           width: '160px',
           margin: '16px auto',
         }}
+        validated={!!roomIdInput}
       >
         <FormGroup>
           <FormLabel>Enter Room ID</FormLabel>
-          <FormControl />
+          <FormControl value={roomIdInput}
+            onChange={(e: FormEvent<HTMLInputElement>) => {
+              setRoomIdInput(e.currentTarget.value);
+              if (roomNotExists) setRoomNotExists(false);
+            }}
+            placeholder='123456'
+            pattern='[0-9]{6}'
+            isValid={!roomNotExists && !!roomIdInput}
+            isInvalid={roomNotExists}
+          />
+          <FormControl.Feedback type='invalid'>
+            {roomNotExists ? `Room ${roomIdInput} doesn't exist.`
+              : '6 digits are allowed.'}
+          </FormControl.Feedback>
         </FormGroup>
-        <Button variant='outline-primary'>
+        <Button variant='outline-primary'
+          onClick={enter} disabled={!roomIdInput}
+        >
           Enter the Room
         </Button>
       </Form>
