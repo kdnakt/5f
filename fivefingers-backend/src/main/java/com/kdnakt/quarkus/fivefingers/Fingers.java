7878acacce7d7024cc4be8f4cab63bda8734cf5d
kdnakt <a.kid.1985@gmail.com>
2020-04-24 06:57:02 +0900

    add fingers API

---
+++ fivefingers-backend/src/main/java/com/kdnakt/quarkus/fivefingers/Fingers.java
@@ -0,0 +1,14 @@
+package com.kdnakt.quarkus.fivefingers;
+
+public class Fingers {
+
+    public int count;
+
+    public Fingers() {
+    }
+
+    public Fingers(int count) {
+        this.count = count;
+    }
+
+}
--- fivefingers-backend/src/main/java/com/kdnakt/quarkus/fivefingers/RoomResource.java
+++ fivefingers-backend/src/main/java/com/kdnakt/quarkus/fivefingers/RoomResource.java
@@ -1,7 +1,9 @@
 package com.kdnakt.quarkus.fivefingers;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -9,6 +11,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import io.vertx.core.metrics.Measured;
 
 @Path("/room")
 public class RoomResource {
@@ -31,4 +35,13 @@ public class RoomResource {
         }
         return Response.ok(id).build();
     }
+
+    @POST
+    @Path("/{id}/fingers")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String send(@PathParam("id") String id, Fingers fingers) {
+        return id + "hoge" + fingers.count;
+    }
+
 }
