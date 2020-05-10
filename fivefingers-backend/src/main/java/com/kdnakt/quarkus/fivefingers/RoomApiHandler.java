16104206e3dd4477a88766eb3533a781dd3bb38b
kdnakt <a.kid.1985@gmail.com>
2020-05-10 15:43:16 +0900

    add RoomApiHandler

---
+++ fivefingers-backend/src/main/java/com/kdnakt/quarkus/fivefingers/RoomApiHandler.java
@@ -0,0 +1,31 @@
+package com.kdnakt.quarkus.fivefingers;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jboss.logging.Logger;
+
+public class RoomApiHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private static final Logger LOG = Logger.getLogger(RoomApiHandler.class);
+
+    @Inject
+    RoomService roomService;
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("input: " + input);
+        String sessionId = RandomStringUtils.randomAlphanumeric(16);
+        return ApiGatewayResponse.builder()
+                .setRawBody(roomService.newRoomId(sessionId))
+                .setHeaders(Collections.singletonMap("Set-Cookie", "sessionId=" + sessionId))
+                .build();
+    }
+
+}
