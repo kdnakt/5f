7b65ffdecd28d74d61555116f712ce9c54190c26
kdnakt <a.kid.1985@gmail.com>
2020-05-24 00:48:44 +0900

    add homepage setting

---
+++ docs/precache-manifest.3787fec86ebd7f67bc29d9bd2fa96483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1386f352a92c3e5cf010ca65eeb6af1c",
+    "url": "/5f/index.html"
+  },
+  {
+    "revision": "f5f3e9f566837ced13e3",
+    "url": "/5f/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b08bd9852ae8ee94b14d",
+    "url": "/5f/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5f3e9f566837ced13e3",
+    "url": "/5f/static/js/2.37f7fe27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/5f/static/js/2.37f7fe27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08bd9852ae8ee94b14d",
+    "url": "/5f/static/js/main.96605e7d.chunk.js"
+  },
+  {
+    "revision": "ff417901885c0f2bdcad",
+    "url": "/5f/static/js/runtime-main.d2d31545.js"
+  }
+]);
