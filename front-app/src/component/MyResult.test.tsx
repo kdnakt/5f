83b590406a31f6aa441bfd4a74e6931999b7afeb
kdnakt <a.kid.1985@gmail.com>
2020-06-10 09:47:50 +0900

    add MyResult.test

---
+++ front-app/src/component/MyResult.test.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import MyResult from './MyResult';
+import { useFinger } from '../data/Fingers';
+import { render } from '@testing-library/react';
+
+
+test('renders MyResult', () => {
+  const fingerType = 'finger'
+  let myCount = 5;
+  const setMyCount = (newCount: number) => myCount = newCount;
+  const myResult = render(<MyResult
+    defs={useFinger(fingerType)}
+    myCount={myCount}
+    setMyCount={setMyCount}
+    socket={undefined}
+    session={{
+      roomId: 'dummyRoomId',
+      sessionId: 'dummySessionId',
+      fingerType: fingerType,
+      nickName: 'dummyNickName',
+    }}
+  />);
+  expect(myResult).toMatchSnapshot();
+});
