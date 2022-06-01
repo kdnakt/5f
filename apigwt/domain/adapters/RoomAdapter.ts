ebece3a1b523b329eef23aeba32259fe77e7e13b
kdnakt <a.kid.1985@gmail.com>
2022-06-02 02:05:13 +0900

    add DdbRoomAdapter class

--- apigwt/domain/adapters/RoomAdapter.ts
+++ apigwt/domain/adapters/RoomAdapter.ts
@@ -1,7 +1,27 @@
+import { DynamoDB } from "aws-sdk";
 import { Room } from "../entities/Room";
 
 export interface IRoomAdapter {
     put(room: Room): Promise<boolean>;
     get(roomId: string): Promise<Room>;
     update(room: Room): Promise<boolean>;
+}
+
+export class DdbRoomAdapter implements IRoomAdapter {
+    private ddb: DynamoDB.DocumentClient;
+
+    constructor(ddb: DynamoDB.DocumentClient) {
+        this.ddb = ddb;
+    }
+
+    put(room: Room): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    get(roomId: string): Promise<Room> {
+        throw new Error("Method not implemented.");
+    }
+    update(room: Room): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+
 }
