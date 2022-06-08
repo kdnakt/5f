f98df0ba55c7faa531acf12e614554a15f13703d
kdnakt <a.kid.1985@gmail.com>
2022-06-08 09:49:24 +0900

    update test

--- apigwt/domain/ports/RoomOutputPort.test.ts
+++ apigwt/domain/ports/RoomOutputPort.test.ts
@@ -2,6 +2,7 @@ import { IRoomAdapter } from "../adapters/RoomAdapter";
 import { Room } from "../entities/Room";
 import { RoomOutputPort } from "./RoomOutputPort";
 
+const id = "999999";
 const type = "like"
 const sessionId = "1234567890";
 
@@ -24,12 +25,29 @@ class DummyRoomAdapter implements IRoomAdapter {
 describe('RoomOutputPort', () => {
   const sut = new RoomOutputPort(new DummyRoomAdapter());
   it('getRoomById', async () => {
-    const id = "999999";
     const room = await sut.getRoom(id);
 
     expect(room.id).toBe(id);
     expect(room.fingerType).toBe(type);
     expect(room.sessionIds).toHaveLength(1);
     expect(room.sessionIds).toContain(sessionId);
-  })
+  });
+
+  it('create', async () => {
+    const room: Room = {
+        id: id,
+        fingerType: type,
+        sessionIds: [sessionId]
+    };
+    const result = await sut.create(room);
+    expect(result).toBe(true);
+  });
+
+  it('update', async () => {
+    const room = await sut.getRoom(id);
+    room.sessionIds.push("0000000000");
+    const result = await sut.update(room);
+    expect(result).toBe(true);
+  });
+
 });
