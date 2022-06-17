d50a8c88f540f44b2ed184f1af878e829f8d987d
kdnakt <a.kid.1985@gmail.com>
2022-06-17 23:26:11 +0900

    add joinRoom test

--- apigwt/domain/ports/RoomInputPort.test.ts
+++ apigwt/domain/ports/RoomInputPort.test.ts
@@ -1,5 +1,5 @@
 import { Room } from "../entities/Room";
-import { NewRoomCommand, RoomInputPort } from "./RoomInputPort";
+import { JoinRoomCommand, NewRoomCommand, RoomInputPort } from "./RoomInputPort";
 import { IRoomOutputPort } from "./RoomOutputPort";
 
 const sessionIds = ["1234567890"];
@@ -25,10 +25,20 @@ describe('RoomInputPort', () => {
   it('makeNewRoom', async () => {
     const command: NewRoomCommand = {
       fingerType: "like"
-    }
+    };
     const res = await sut.makeNewRoom(command);
     expect(res.success).toBe(true);
     expect(res.roomId).toBeTruthy();
+    expect(res.sessionId).toBeTruthy();
+  });
+
+  it('joinRoom', async () => {
+    const command: JoinRoomCommand = {
+      roomId: '987654'
+    };
+    const res = await sut.joinRoom(command);
+    expect(res.success).toBe(true);
+    expect(res.fingerType).toBe('like');
     expect(res.sessionId).toBeTruthy();
   });
 });
