f165dcacbabe2a14356965e3293c7c71d083d586
kdnakt <a.kid.1985@gmail.com>
2022-06-30 08:16:38 +0900

    remove old code

--- apigwt/ApiHandler.ts
+++ apigwt/ApiHandler.ts
@@ -1,10 +1,7 @@
-import { APIGatewayProxyHandler, Context } from 'aws-lambda';
+import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 import { DynamoDB } from 'aws-sdk';
-import { lastUpdated } from './util/lastUpdated';
-import { randomRoomId } from './domain/values/RoomId';
-import { randomSessionId } from './domain/values/SessionId';
 import { JoinRoomCommand, NewRoomCommand, RoomInputPort } from './domain/ports/RoomInputPort';
 import { RoomOutputPort } from './domain/ports/RoomOutputPort';
 import { DdbRoomAdapter } from './domain/adapters/RoomAdapter';
@@ -17,69 +14,6 @@ const corsOrigin = process.env.CORS_ORIGIN;
 const roomAdapter = new DdbRoomAdapter(roomTable, db);
 const roomOutputPort = new RoomOutputPort(roomAdapter);
 const roomInputPort = new RoomInputPort(roomOutputPort);
-
-const exists = async (id: string) => {
-  let res = false;
-  await db.get({
-    TableName: roomTable,
-    Key: {
-      RoomId: id
-    }
-  }).promise().then(() => res = true);
-  return res;
-}
-
-const newRoomId = async (sessionId: string, type: string) => {
-  let fingerType = type;
-  if (fingerType !== 'finger' && fingerType !== 'like') {
-    fingerType = 'finger'; // use default
-  }
-  const id = randomRoomId();
-  await db.put({
-    TableName: roomTable,
-    Item: {
-      RoomId: id,
-      FingerType: fingerType,
-      SessionIds: [sessionId],
-      LastUpdated: lastUpdated(),
-    },
-    ConditionExpression: 'attribute_not_exists(SessionIds)',
-  }).promise().then(() => {
-    console.log('newRoomId:', id);
-  }).catch(e => {
-    console.log(e);
-  });
-  return [id, fingerType];
-}
-
-const addSession = async (roomId: string, sessionId: string, _context: Context) => {
-  const data = await db.get({
-    TableName: roomTable,
-    Key: {
-      RoomId: roomId
-    },
-    ProjectionExpression: 'SessionIds, LastUpdated, FingerType',
-  }).promise();
-  if (!data.Item.SessionIds.includes(sessionId)) {
-    data.Item.SessionIds.push(sessionId);
-    db.update({
-      TableName: roomTable,
-      Key: {
-        RoomId: roomId,
-      },
-      UpdateExpression: 'SessionIds = :sids, LastUpdated = :lu',
-      ExpressionAttributeValues: {
-        ':sids': data.Item.SessionIds,
-        ':lu': lastUpdated(),
-      }
-    }).promise();
-  } else {
-    const errMsg = 'session already exists';
-    console.log(errMsg);
-    _context.fail(errMsg);
-  }
-  return data.Item.FingerType;
-}
 
 export const newRoom: APIGatewayProxyHandler = async (event, _context) => {
   console.log('event:', event);
